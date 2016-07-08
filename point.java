77370c07074cdbe4367e95ea7e5a6e7c0669f16a
allforlove901 <brettleenelson@gmail.com>
2016-07-08 14:51:45 -0700

    Cleaned up the code

--- point.java
+++ point.java
@@ -1,14 +1,24 @@
-
+/**
+ * Class to implement 3d point object
+ * Author:	Brett Nelson
+ * Date:	7/7/2016
+ * /
 public class point {
 	
-	//
+	//instance variables to represent standard cartesian coordinates
 	double x = 0, y = 0, z = 0;
 	
+	//no-args constructor
 	public point(){
 		//nothing here
 	}
 	
-	public point(double x, double y, double z){
+	//Constructor
+	//@param x - double type representing x component
+	//@param y - double type representing y component
+	//@param z - double type representing z component
+	public point(double x, double y, double z)
+	{
 		this.x = x;
 		this.y = y;
 		this.z = z;
