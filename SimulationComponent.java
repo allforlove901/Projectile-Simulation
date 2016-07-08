25fdbe3a4a3c1e44c868ab1964ff3539f00ea12a
allforlove901 <brettleenelson@gmail.com>
2016-07-08 14:46:38 -0700

    Cleaned up the code

--- SimulationComponent.java
+++ SimulationComponent.java
@@ -2,25 +2,31 @@ import javax.swing.JComponent;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
 
 /**
-   Displays a bull's eye.
+   Class to Display visual of projectiles motion
+   @extends JComponent
+   Author:	Brett Nelson
+   Date:	7/7/2016
  */
 public class SimulationComponent extends JComponent
 {
 	point p1, p2;
+	
+	//constructor
+	//@param p1 - position of second impact
+	//@param p2 - position of first impact
 	public SimulationComponent(point p1, point p2){
 		this.p1 = p1;
 		this.p2 = p2;
 	}
 	
-   public void paintComponent(Graphics g)
-   {
-      Graphics2D g2 = (Graphics2D) g;
-      //Ellipse2D.Double marker = new Ellipse2D.Double(currentPosition.x, currentPosition.y, 5, 5);
-      g2.setColor(Color.BLACK);
-      //g2.fill(marker);
-      g2.drawLine((int)p2.x, (int)p2.y, (int)p1.x, (int)p1.y);
-   }
+	//method to create visual component for trajectory
+	//@param g - graphics interface
+	public void paintComponent(Graphics g)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(Color.BLACK);
+		g2.drawLine((int)p2.x, (int)p2.y, (int)p1.x, (int)p1.y);
+	}
 }
